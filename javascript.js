d1fc5837584ac487eb63736fb589e24e49eb2bef
AbdulRhman Hassn <bodahassan12@gmail.com>
2025-08-12 10:01:11 +0300

    Make the library work with no books at start and make the add book button work and change its styling

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const mainContainer = document.querySelector('main');
 const booksContaier = document.querySelector('.book-container');
 const allBooks = Array.from(booksContaier.children);
+const addBookButton = document.getElementById("add-book");
  
 
 function updateShelves() {
@@ -8,8 +9,8 @@ function updateShelves() {
   // Clear all existing shelves from the main container at the start of the function
   document.querySelectorAll('.book-shelf').forEach(shelf => shelf.remove());
 
-  // 40px width + 10px gap
-  const bookWidth = 40 + 10;
+  // 40px width + 5px gap
+  const bookWidth = 40 + 2.5;
   const containerWidth = mainContainer.clientWidth + 10;
   const booksPerShelf = Math.floor(containerWidth / bookWidth);
   let currentShelf;
@@ -39,8 +40,23 @@ window.addEventListener("resize", updateShelves);
 // Run on load
 updateShelves();
 
-allBooks.forEach(book => {
-  book.addEventListener("click", () => {
-    book.classList.toggle("book-expanded");
+// Add a new book and make it expandable
+addBookButton.addEventListener("click", () => {
+  const newBook = document.createElement("div");
+  if (booksContaier.lastChild && booksContaier.lastChild.tagName === "DIV") {
+    const bookShelf = booksContaier.lastChild;
+    bookShelf.appendChild(newBook);
+  }
+  else {
+    const bookShelf = document.createElement("div");
+    booksContaier.appendChild(bookShelf);
+    bookShelf.appendChild(newBook);
+  }
+  allBooks.push(newBook);
+  newBook.classList.add("book" + (allBooks.length + 1));
+  newBook.textContent = allBooks.length;
+  updateShelves();
+  newBook.addEventListener("click", () => {
+    newBook.classList.toggle("book-expanded");
   });
 });
